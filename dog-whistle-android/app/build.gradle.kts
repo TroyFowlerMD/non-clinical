4df18a7db0707950f15d5df46ca5f9f504b76179
TroyFowlerMD <troyfowlermd@gmail.com>
2026-06-23 11:51:49 -0400

    Fix Dog Whistle Android theme dependency

--- dog-whistle-android/app/build.gradle.kts
+++ dog-whistle-android/app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(composeBom)
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
